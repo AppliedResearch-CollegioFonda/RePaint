47a6c383d1856b2a369f34ffa20342f755f44664
AppliedResearch-CollegioFonda <155445524+AppliedResearch-CollegioFonda@users.noreply.github.com>
2024-01-20 14:33:50 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,18 +4,19 @@
 mkdir -p data/pretrained
 cd data/pretrained
 
-wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_classifier.pt # Trained by OpenAI
-wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion.pt # Trained by OpenAI
+#wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_classifier.pt # Trained by OpenAI
+#wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion.pt # Trained by OpenAI
+wget https://openaipublic.blob.core.windows.net/diffusion/jul-2021/256x256_diffusion_uncond.pt
 
-gdown https://drive.google.com/uc?id=1norNWWGYP3EZ_o05DmoW1ryKuKMmhlCX
-gdown https://drive.google.com/uc?id=1QEl-btGbzQz6IwkXiFGd49uQNTUtTHsk
+#gdown https://drive.google.com/uc?id=1norNWWGYP3EZ_o05DmoW1ryKuKMmhlCX
+#gdown https://drive.google.com/uc?id=1QEl-btGbzQz6IwkXiFGd49uQNTUtTHsk
 )
 
 # data
 (
-gdown https://drive.google.com/uc?id=1Q_dxuyI41AAmSv9ti3780BwaJQqwvwMv
-unzip data.zip
-rm data.zip
+#gdown https://drive.google.com/uc?id=1Q_dxuyI41AAmSv9ti3780BwaJQqwvwMv
+#unzip data.zip
+#rm data.zip
 
 #gdown https://drive.google.com/drive/folders/1QHiCnwMK-wKkF73bJ-ne20pfWB-PulTE?usp=drive_link -O /tmp/folder --folder
 )
